45859fac67a120cf42e4d3ea86c2668992f2911e
mark harrison <darthmalick@gmail.com>
2022-06-13 13:05:40 -0500

    commit tree c94fce115fe878af42bca7169ffd9dbebb1c4a8c

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; 
+function destructivelyAppendCat() {
+     cats.push("Ralph") 
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+     cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+cats.shift();
+}
+function appendCat(){
+    const allcats = [...cats]
+    allcats.push("Broom")
+    return allcats;
+}
+function prependCat(){
+    const allcats = [...cats]
+    allcats.unshift("Arnold")
+    return allcats;
+}
+function removeLastCat(){
+    return cats.slice(0, -1);
+ }
+ function removeFirstCat(){
+     return cats.slice(-2)
+ }
